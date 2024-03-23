0308411566826209462d7d03e96f293ca5b16af0
Michael Florya <mih2303@mail.ru>
2024-03-23 15:55:51 +0300

    add proxy files

---
+++ proxy/proxy.py
@@ -0,0 +1,30 @@
+from random import choice
+import requests
+
+# Указываем URL, к которому будем отправлять запрос для тестирования прокси
+url = 'http://httpbin.org/ip'
+
+# Открываем файл с прокси и читаем его
+with open('proxy.txt') as file:
+    # Считываем содержимое файла и разделяем его на строки
+    proxy_file = file.read().split('\n')
+
+    for _ in range(14):
+        try:
+            # Выбираем случайный прокси из списка и удаляем лишние пробелы
+            ip = choice(proxy_file).strip()
+
+            # Формируем словарь с прокси для http и https
+            proxy = {
+                'http': f'http://{ip}',
+                'https': f'https://{ip}'
+            }
+            # Выполняем GET-запрос с использованием выбранного прокси
+            response = requests.get(url=url, proxies=proxy)
+
+            # Выводим результат в случае успешного подключения
+            print(response.json(), 'Success connection')
+        except Exception as _ex:
+
+            # В случае неудачи пропускаем текущую итерацию
+            continue
